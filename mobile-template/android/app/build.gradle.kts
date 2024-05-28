380ad65a4b7d13d641282a794c43e227cd731403
Vladas Zakrevskis <146100@gmail.com>
2024-05-28 22:57:50 +0300

    min sdk 21

--- mobile-template/android/app/build.gradle.kts
+++ mobile-template/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.TEST_MOBILE_PROJECT_NAME_SNAKE_CASE"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
