f1fb23d92672bb588dadab26adc17b5e1ee12c72
Vladas Zakrevskis <146100@gmail.com>
2026-02-21 21:10:52 +0200

    wip

--- test-mobile-game-android/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(target_os = "android")]
-#[no_mangle]
-fn android_main(_app: winit::platform::android::activity::AndroidApp) {
-    dbg!("Hello");
-}
--- test-mobile-game/src/lib.rs
+++ test-mobile-game/src/lib.rs
@@ -1,4 +1,32 @@
-#[unsafe(no_mangle)]
-extern "C" fn test_engine_start_app() {
-    dbg!("Hello world!");
+#![allow(incomplete_features)]
+#![feature(specialization)]
+#![feature(arbitrary_self_types)]
+
+use test_engine::{
+    App,
+    refs::{Own, Weak},
+    ui::{Label, Setup, View, ViewData, view},
+};
+
+#[view]
+struct MobileView {
+    #[init]
+    label: Label,
 }
+
+impl Setup for MobileView {
+    fn setup(self: Weak<Self>) {
+        self.label.set_text("Hello").place().back();
+    }
+}
+
+#[derive(Default)]
+struct MobileApp {}
+
+impl App for MobileApp {
+    fn make_root_view(&self) -> Own<dyn View> {
+        MobileView::new()
+    }
+}
+
+test_engine::register_app!(MobileApp);
